b2464599e88c8adae4c34b81876963e343c5cd82
haris-ism <amin.haris3323@gmail.com>
2022-10-23 21:28:44 +0700

    adding query func

--- controller/mainController.go
+++ controller/mainController.go
@@ -6,36 +6,61 @@ import (
 	"net/http"
 	"os"
 	"time"
-
 	"github.com/gin-gonic/gin"
 	jwt "github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
 // var asd int
-
-func (t *Repo) SignUp(c *gin.Context) {
+func QueryFind(t *Repo,c *gin.Context,condition string,value string) interface{}{
 	var user model.Accounts
+
+	// var user interface{}
+	// user=model.Accounts{}
+	// var asd interface{}
+	// asd="aaa"
+	// log.Println("type asd",reflect.TypeOf(asd))
+	// asd=1
+	// log.Println("type asd",reflect.TypeOf(asd))
+	// asd=model.Accounts{}
+	// log.Println("type asd",reflect.TypeOf(asd))
+	// log.Println("type user",user.(model.Accounts).Email)
+	// // log.Println("type user1",reflect.TypeOf(user1.Email),user1.Email)
+
+	// log.Println("ieu find na ",condition,value)
+	// switch condition{
+	// 	case "id = ?":
+	// 	user=model.Users{}
+	// }
+
+	if err := t.DB.Find(&user,condition, value).Error; err != nil {
+		log.Println("failed to get data")
+		c.JSON(500, gin.H{
+			"message": "failed to find email",
+		})
+		return false
+	}
+	if user.ID != 0 {
+		log.Println("data not Found")
+		c.JSON(500, gin.H{
+			"message": "email is already used",
+		})
+		return false
+	}
+	return true
+}
+func (t *Repo) SignUp(c *gin.Context) {
+	// var user model.Accounts
 	body := model.Sign{}
 	//method to get body of request
 	if err := c.BindJSON(&body); err != nil {
 		log.Println(err)
 		return
 	}
-	if err := t.DB.Find(&user,"email = ?", body.Email).Error; err != nil {
-		log.Println("failed to get data")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "failed to find email",
-		})
+	if QueryFind(t,c,"email = ?",body.Email)==false{
 		return
 	}
-	if user.ID != 0 {
-		log.Println("data not Found")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "email is already used",
-		})
-		return
-	}
+	log.Println("query success euy")
 	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
 	if err != nil {
 		log.Println("failed to hash password")
@@ -48,7 +73,7 @@ func (t *Repo) SignUp(c *gin.Context) {
 		log.Println(err1)
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
+	c.JSON(201, gin.H{
 		"message": "write success",
 		"data":    account,
 	})
@@ -63,23 +88,13 @@ func (t *Repo) SignIn(c *gin.Context) {
 		return
 	}
 
-	if err := t.DB.Find(&user,"email = ?", body.Email).Error; err != nil {
-		log.Println("failed to get data")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "failed to find email",
-		})
+	if QueryFind(t,c,"email = ?",body.Email)==false{
 		return
 	}
-	if user.ID == 0 {
-		log.Println("data not Found")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "email and password combination is invalid",
-		})
-		return
-	}
-	
+	log.Println("query success euy")
+
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password)); err != nil {
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "password invalid",
 		})
 		return
@@ -105,10 +120,15 @@ func (t *Repo) SignIn(c *gin.Context) {
 func (t *Repo) Get(c *gin.Context) {
 	var user []model.Users
 	var getAll []model.UsersJoinCreditCards
+
 	query:="users.id, users.name,users.job, credit_cards.credit_card_number,credit_cards.ammount,credit_cards.limit,credit_cards.bank,credit_cards.updated_at"
 	join:="left join credit_cards on credit_cards.users_id = users.id and credit_cards.deleted_at is null"
+	
 	if err := t.DB.Model(&user).Select(query).Joins(join).Scan(&getAll).Error; err != nil {
 		log.Println("failed to get data")
+		c.JSON(500, gin.H{
+			"message": "failed to get data",
+		})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
@@ -121,17 +141,16 @@ func (t *Repo) GetOne(c *gin.Context) {
 	id := c.Param("id")
 	var user model.Users
 
-	if err := t.DB.Find(&user, id).Error; err != nil {
+	if err := t.DB.Find(&user,"id = ?", id).Error; err != nil {
 		log.Println("failed to get data")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to get data",
-			"info":    err,
 		})
 		return
 	}
 	if user.ID == 0 {
 		log.Println("data not Found")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "no data",
 		})
 		return
@@ -147,6 +166,9 @@ func (t *Repo) Post(c *gin.Context) {
 	//method to get body of request
 	if err := c.BindJSON(&body); err != nil {
 		log.Println(err)
+		c.JSON(500, gin.H{
+			"message": "failed to get body data",
+		})
 		return
 	}
 
@@ -157,6 +179,9 @@ func (t *Repo) Post(c *gin.Context) {
 	// method to post to DB
 	if err1 := t.DB.Create(&user).Error; err1 != nil {
 		log.Println(err1)
+		c.JSON(500, gin.H{
+			"message": "failed to create data",
+		})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
@@ -171,19 +196,22 @@ func (t *Repo) RegistrationCC(c *gin.Context) {
 	//method to get body of request
 	if err := c.BindJSON(&body); err != nil {
 		log.Println(err)
+		c.JSON(500, gin.H{
+			"message": "failed to get body data",
+		})
 		return
 	}
 
 	if err := t.DB.Find(&user, body.UsersID).Error; err != nil {
 		log.Println("failed to find users id", err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to find users id",
 		})
 		return
 	}
 	if user.ID == 0 {
 		log.Println("register cc: users id not found")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "users id not found",
 		})
 		return
@@ -191,14 +219,14 @@ func (t *Repo) RegistrationCC(c *gin.Context) {
 	var creditCards model.CreditCards
 	if err := t.DB.Find(&creditCards,"users_id = ?",user.ID).Error; err != nil {
 		log.Println("failed to find users id", err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to find users id",
 		})
 		return
 	}
 	if creditCards.UsersID != 0 {
 		log.Println("register cc: credit card already registered before")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "credit card already registered before",
 		})
 		return
@@ -209,7 +237,7 @@ func (t *Repo) RegistrationCC(c *gin.Context) {
 	// method to post to DB
 	if err := t.DB.Create(&creditCards).Error; err != nil {
 		log.Println("error when registering cc to the database euy", err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to regist credit card",
 		})
 		return
@@ -227,16 +255,22 @@ func (t *Repo) Put(c *gin.Context) {
 
 	if err := c.BindJSON(&body); err != nil {
 		log.Println(err)
+		c.JSON(500, gin.H{
+			"message": "failed to get body data",
+		})
 		return
 	}
 	
 	if err := t.DB.Find(&user, id).Error; err != nil {
 		log.Println(err)
+		c.JSON(500, gin.H{
+			"message": "failed to find users",
+		})
 		return
 	}
 	if user.ID == 0 {
 		log.Println("no data")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "user id not found",
 		})
 		return
@@ -245,7 +279,7 @@ func (t *Repo) Put(c *gin.Context) {
 	user.Job = body.Job
 	if err := t.DB.Save(&user).Error; err != nil {
 		log.Println(err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "update failed",
 		})
 		return
@@ -267,28 +301,28 @@ func (t *Repo) UpdateCreditCards(c *gin.Context) {
 	}
 	if err := t.DB.Find(&user, body.UsersID).Error; err != nil {
 		log.Println(err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to search users id",
 		})
 		return
 	}
 	if user.ID == 0 {
 		log.Println("no data")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "user id not found",
 		})
 		return
 	}
 	if err := t.DB.Find(&creditCard, "users_id = ?", body.UsersID).Error; err != nil {
 		log.Println(err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to search credit card id",
 		})
 		return
 	}
 	if creditCard.ID == 0 {
 		log.Println("no data")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "credit card is not registered",
 		})
 		return
@@ -300,7 +334,7 @@ func (t *Repo) UpdateCreditCards(c *gin.Context) {
 
 	if err := t.DB.Save(&creditCard).Error; err != nil {
 		log.Println(err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "update failed",
 		})
 		return
@@ -316,21 +350,21 @@ func (t *Repo) Delete(c *gin.Context) {
 	var user model.Users
 	if err := t.DB.Find(&user, id).Error; err != nil {
 		log.Println("delete cc failed :",err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to get data",
 		})
 		return
 	}
 	if user.ID == 0 {
 		log.Println("users id not Found")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "users id not found",
 		})
 		return
 	}
 	if err := t.DB.Delete(&user, id).Error; err != nil {
 		log.Println("users delete failed :",err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to delete data",
 		})
 		return
@@ -347,7 +381,7 @@ func (t *Repo) DeleteCC(c *gin.Context) {
 	
 	if err := t.DB.Find(&creditCards,"users_id = ?", id).Error; err != nil {
 		log.Println("delete cc failed :",err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to get data",
 		})
 		return
@@ -355,14 +389,14 @@ func (t *Repo) DeleteCC(c *gin.Context) {
 
 	if creditCards.UsersID == 0 {
 		log.Println("credit card not Found")
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(400, gin.H{
 			"message": "credit card not Found",
 		})
 		return
 	}
 	if err := t.DB.Delete(&creditCards, "users_id = ?", id).Error; err != nil {
 		log.Println("failed to delete data: ",err)
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(500, gin.H{
 			"message": "failed to delete data",
 		})
 		return
--- model/table.go
+++ model/table.go
@@ -5,7 +5,7 @@ import (
 
 	"gorm.io/gorm"
 )
-
+type MethodInterface interface{}
 type Accounts struct {
 	gorm.Model
 	Email    string `json:"email" binding:"required" gorm:"unique"`
